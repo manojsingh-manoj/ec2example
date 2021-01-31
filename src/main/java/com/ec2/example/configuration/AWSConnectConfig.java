1e953b6d8d576496a88cd1480290d87bcc55c2a4
manojsigh <manojsingh.manoj@gmail.com>
2021-01-31 08:39:53 +0530

    x

--- src/main/java/com/ec2/example/configuration/AWSConnectConfig.java
+++ src/main/java/com/ec2/example/configuration/AWSConnectConfig.java
@@ -1,7 +1,11 @@
 package com.ec2.example.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSCredentialsProviderChain;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -13,23 +17,18 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 @Configuration
 public class AWSConnectConfig {
 
-	/*
-	 * @Bean public AmazonS3 gets3Client() { AmazonS3 s3Client = null; try {
-	 * AWSCredentialsProvider provider = new AWSCredentialsProviderChain(new
-	 * InstanceProfileCredentialsProvider(true),new
-	 * ProfileCredentialsProvider("default")); s3Client =
-	 * AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).
-	 * withCredentials(provider).build();
-	 * 
-	 * }catch(Exception e) { e.printStackTrace(); } return s3Client; }
-	 */
-	
-	
-	public AmazonS3 S3Client() {
-		 AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                 .withCredentials(new ProfileCredentialsProvider())
-                 .withRegion(Regions.US_EAST_2)
-                 .build();
-			return s3Client;
+	@Bean
+	public AmazonS3 gets3Client() {
+		AmazonS3 s3Client = null;
+		try {
+			AWSCredentialsProvider provider = new AWSCredentialsProviderChain(new InstanceProfileCredentialsProvider(true),new ProfileCredentialsProvider());
+			s3Client  = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).withCredentials(provider).build();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return s3Client;
 	}
+	
+
 }
